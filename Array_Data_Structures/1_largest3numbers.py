538da9ab0ba9a8db1dc54a71b5f5d55c30ab191d
Tavleen Bajwa <86622630+tavleenbajwa8@users.noreply.github.com>
2023-02-21 17:19:39 +0530

    Update 1_largest3numbers.py

--- Array_Data_Structures/1_largest3numbers.py
+++ Array_Data_Structures/1_largest3numbers.py
@@ -1,5 +1,6 @@
 # Find the largest three distinct elements in an array
 
+#Code Example-1
 import sys
  
 def largest_3(arr,n):
@@ -27,3 +28,14 @@ arr = [2,6,5,3,1,9,7,11]
 n = len(arr)
 
 largest_3(arr, n)
+
+
+#Code Example 2
+
+arr = [40,6,50,3,1,90,7,11]
+n = len(arr)
+
+sorted_list = sorted(arr)
+print(sorted_list[-1:-4:-1])
+
+#Code Example 3
